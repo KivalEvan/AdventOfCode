9d2bae8648c2464b7f267048006b11bf130b25ca
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2023-12-09 14:35:27 +0800

    i somehow made ts faster than c

--- 2023/09/ts/main.ts
+++ 2023/09/ts/main.ts
@@ -3,28 +3,31 @@ import { run } from 'utils/run.ts';
 /** If part 2 test input has completely different input, set this to `true`. */
 export const HAS_ALTERNATE = false;
 
-function difference(r: number[]): number[] {
-   const ary = [];
-   for (let i = 1; i < r.length; i++) ary[i - 1] = r[i] - r[i - 1];
-   return ary;
+function difference(r: number[], sz: number): number[] {
+   for (let i = 0; i < sz; i++) r[i] = r[i + 1] - r[i];
+   return r;
 }
 
-function extrapolate(r: number[]): number {
-   if (r.every((x) => x === 0)) return 0;
-   return extrapolate(difference(r)) + r[r.length - 1];
+function extrapolate(r: number[], sz: number): number {
+   const last = r[r.length - 1];
+   let m = 0;
+   for (let i = 0; i < sz; i++) m = m | r[i];
+   if (!m) return 0;
+   sz--;
+   return extrapolate(difference(r, sz), sz) + last;
 }
 
 export function part1(input: string): string {
    const parsed = input.split('\n').map((str) => str.split(' ').map(Number));
    let res = 0;
-   for (const history of parsed) res += extrapolate(history);
+   for (const history of parsed) res += extrapolate(history, history.length);
    return res.toString();
 }
 
 export function part2(input: string): string {
    const parsed = input.split('\n').map((str) => str.split(' ').map(Number));
    let res = 0;
-   for (const history of parsed) res += extrapolate(history.reverse());
+   for (const history of parsed) res += extrapolate(history.reverse(), history.length);
    return res.toString();
 }
 
